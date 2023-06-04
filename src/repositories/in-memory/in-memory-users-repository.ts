2b40a1edd462abcb446105f9c344ffbf3cf6ce81
Kayke-Fujinaka <kaykealvesfujinaka@gmail.com>
2023-06-04 16:04:34 -0300

    feat: creates find by id method

--- src/repositories/in-memory/in-memory-users-repository.ts
+++ src/repositories/in-memory/in-memory-users-repository.ts
@@ -24,4 +24,10 @@ export class InMemoryUsersRepository implements UsersRepository {
 
     return !user ? null : user
   }
+
+  async findById(id: string): Promise<User | null> {
+    const user = this.items.find((item) => item.id === id)
+
+    return !user ? null : user
+  }
 }
