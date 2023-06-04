9d61507da3bd215825ccf9c2494932d7163522ef
Kayke-Fujinaka <kaykealvesfujinaka@gmail.com>
2023-06-04 16:06:49 -0300

    test: should be able to get user profile

---
+++ src/use-cases/get-user-profile.spec.ts
@@ -0,0 +1,29 @@
+import { hash } from 'bcryptjs'
+import { beforeEach, describe, expect, it } from 'vitest'
+
+import { InMemoryUsersRepository } from '@/repositories/in-memory/in-memory-users-repository'
+import { GetUserProfile } from './get-user-profile'
+
+let usersRepository: InMemoryUsersRepository
+let sut: GetUserProfile
+
+describe('Get User Profile Use Case', () => {
+  beforeEach(() => {
+    usersRepository = new InMemoryUsersRepository()
+    sut = new GetUserProfile(usersRepository)
+  })
+
+  it('should be able to get user profile', async () => {
+    const createdUser = await usersRepository.create({
+      name: 'John Doe',
+      email: 'johndoe@example.com',
+      password_hash: await hash('123456', 6),
+    })
+
+    const { user } = await sut.execute({
+      userId: createdUser.id,
+    })
+
+    expect(user.name).toEqual('John Doe')
+  })
+})
