63e6384f9a5cad502c3f8c65728054f9ab7d83f6
Kayke-Fujinaka <kaykealvesfujinaka@gmail.com>
2023-06-08 14:34:41 -0300

    test: check in use case

---
+++ src/use-cases/check-in.spec.ts
@@ -0,0 +1,23 @@
+import { beforeEach, describe, expect, it } from 'vitest'
+
+import { InMemoryCheckInRepository } from '@/repositories/in-memory/in-memory-check-in-repository'
+import { CheckInUseCase } from './check-in'
+
+let checkInsRepository: InMemoryCheckInRepository
+let sut: CheckInUseCase
+
+describe('Check In Use Case', () => {
+  beforeEach(() => {
+    checkInsRepository = new InMemoryCheckInRepository()
+    sut = new CheckInUseCase(checkInsRepository)
+  })
+
+  it('should be able to check in', async () => {
+    const { checkIn } = await sut.execute({
+      gymId: 'gym-01',
+      userId: 'user-01',
+    })
+
+    expect(checkIn.id).toEqual(expect.any(String))
+  })
+})
