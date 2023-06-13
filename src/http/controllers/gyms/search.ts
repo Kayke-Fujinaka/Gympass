0cf757e27cea9c6c12fc02b384cc2a4f1b3a7240
Kayke-Fujinaka <kaykealvesfujinaka@gmail.com>
2023-06-13 15:41:29 -0300

    feat: search gyms controller

---
+++ src/http/controllers/gyms/search.ts
@@ -0,0 +1,22 @@
+import { FastifyReply, FastifyRequest } from 'fastify'
+import { z } from 'zod'
+
+import { makeSearchGymsUseCase } from '@/use-cases/factories/search-gyms-use-case'
+
+export async function search(request: FastifyRequest, reply: FastifyReply) {
+  const searchGymsQuerySchema = z.object({
+    query: z.string(),
+    page: z.coerce.number(),
+  })
+
+  const { query, page } = searchGymsQuerySchema.parse(request.body)
+
+  const searchGymsUseCase = makeSearchGymsUseCase()
+
+  await searchGymsUseCase.execute({
+    query,
+    page,
+  })
+
+  return reply.status(201).send()
+}
