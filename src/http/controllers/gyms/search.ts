7161b08a894ad15e25808724918fa429b4a93fa3
Kayke-Fujinaka <kaykealvesfujinaka@gmail.com>
2023-06-13 15:59:01 -0300

    feat: adds new validations

--- src/http/controllers/gyms/search.ts
+++ src/http/controllers/gyms/search.ts
@@ -6,7 +6,7 @@ import { makeSearchGymsUseCase } from '@/use-cases/factories/search-gyms-use-cas
 export async function search(request: FastifyRequest, reply: FastifyReply) {
   const searchGymsQuerySchema = z.object({
     query: z.string(),
-    page: z.coerce.number(),
+    page: z.coerce.number().min(1).default(1),
   })
 
   const { query, page } = searchGymsQuerySchema.parse(request.query)
