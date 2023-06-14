d49a130612fddb9fe22924f5e52ea7a36910efc2
Kayke-Fujinaka <kaykealvesfujinaka@gmail.com>
2023-06-14 09:21:24 -0300

    feat: adds middleware to validate user when creates gyms

--- src/http/middlewares/verify-user-role.ts
+++ src/http/middlewares/verify-user-role.ts
@@ -1,6 +1,6 @@
 import { FastifyReply, FastifyRequest } from 'fastify'
 
-export async function verifyUserRole(roleToVerify: 'ADMIN' | 'MEMBER') {
+export function verifyUserRole(roleToVerify: 'ADMIN' | 'MEMBER') {
   return (request: FastifyRequest, reply: FastifyReply) => {
     const { role } = request.user
 
