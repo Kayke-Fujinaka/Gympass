7ef198113cb289dd8a11d8d7e3c38f8e6c1e82de
Kayke-Fujinaka <kaykealvesfujinaka@gmail.com>
2023-06-17 15:43:31 -0300

    feat: change password use case

---
+++ src/use-cases/change-password.ts
@@ -0,0 +1,30 @@
+import { compare, hash } from 'bcryptjs'
+
+import { UsersRepository } from '@/repositories/users-repository'
+import { InvalidCredentialsError } from './errors/invalid-credentials'
+
+interface ChangePasswordUseCaseRequest {
+  userId: string
+  password: string
+  newPassword: string
+}
+
+export class ChangePasswordUseCase {
+  constructor(private usersRepository: UsersRepository) {}
+
+  async execute({
+    userId,
+    password,
+    newPassword,
+  }: ChangePasswordUseCaseRequest): Promise<void> {
+    const user = await this.usersRepository.findById(userId)
+
+    if (!user) throw new InvalidCredentialsError()
+
+    const doesPasswordMatches = await compare(password, user.password_hash)
+
+    if (!doesPasswordMatches) throw new InvalidCredentialsError()
+
+    user.password_hash = await hash(newPassword, 6)
+  }
+}
